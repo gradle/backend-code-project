47daa04c56963a8ae3b9b3381c5bab4abdd6a481
Etienne Studer <etienne@studer.nu>
2023-06-09 10:35:53 +0200

    Create backend code project scaffolding

---
+++ server/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    idea
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("com.linecorp.armeria:armeria-bom:1.27.0"))
+    implementation("com.linecorp.armeria:armeria")
+    implementation("com.linecorp.armeria:armeria-logback")
+
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
+    runtimeOnly("org.slf4j:log4j-over-slf4j:2.0.12")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+}
+
+tasks.register<JavaExec>("start") {
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("com.gradle.develocity.assignment.MyServer")
+}
