5e06ed4aaac32ec3a1b3d41d26136cebebbc9bab
Etienne Studer <etienne@studer.nu>
2023-06-09 10:35:53 +0200

    Create backend code project scaffolding

---
+++ server/src/main/java/com/gradle/enterprise/assignment/MyServer.java
@@ -0,0 +1,21 @@
+package com.gradle.enterprise.assignment;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+
+import java.util.concurrent.CompletableFuture;
+
+public final class MyServer {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        ServerBuilder sb = Server.builder();
+        sb.http(5555);
+        sb.service("/", (ctx, req) -> HttpResponse.of("Gradle Enterprise Backend Code Project"));
+        Server server = sb.build();
+        CompletableFuture<Void> future = server.start();
+        future.join();
+    }
+
+}
